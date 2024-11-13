e4c434bc6f199c6f2e840eba0ae77d9d0fc42989
Dueris <122416109+Dueris@users.noreply.github.com>
2024-11-13 21:56:57 +0000

    Remove uneeded fallback

--- src/main/java/me/dueris/eclipse/EclipsePlugin.java
+++ src/main/java/me/dueris/eclipse/EclipsePlugin.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public final class EclipsePlugin extends JavaPlugin {
-	public static final List<PaperSimplePluginClassLoader> CLASSLOADERS = new ArrayList<>();
 	public static final AtomicReference<PaperPluginParent.PaperServerPluginProvider> CURRENT_OPERATING_PROVIDER = new AtomicReference<>();
 	public static boolean eclipse$allowsJars = false;
 }
