c200b43a2e3720119e11d07ac082801bbd23aa8e
Dueris <122416109+Dueris@users.noreply.github.com>
2024-12-19 21:22:20 +0000

    Add package info for launch patches

---
+++ src/main/java/io/github/dueris/eclipse/loader/launch/patch/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains hard-coded patches for the source that are
+ * simple enough to not be needed as mixins, and as tests for hardcoded
+ * patches of the ClassNode.
+ */
+package io.github.dueris.eclipse.loader.launch.patch;
